6db7caadfd07a7080439903c8d414bc115512df3
Darshan magdum <Darshanmagdum79@gmail.com>
2024-07-02 13:22:45 +0530

    cahnhes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Login from "../../components/Development/Login"
 import Signup from '../../components/Development/Signup';
 import ForgotPassword from '../../components/Development/Forgotpassword';
@@ -29,10 +29,10 @@ import ViewBranches from '../../components/Screen/Admin/ViewBranches';
 import VendorRegistration from '../../components/Screen/Admin/VendorRegistration';
 import ViewVendors from '../../components/Screen/Admin/ViewVendors';
 import ViewUsers from '../../components/Screen/Admin/ViewUsers';
-import VendorMemberSiderMenu from '../../components/Screen/VendorMember/VendorSiderMenu';
 import ViewFoodItems from '../../components/Screen/VendorMember/ViewFoodItems';
 import EditVendorMemberProfile from '../../components/Screen/VendorMember/EditVendorMemberProfile';
 import VendorMemberProfileView from '../../components/Screen/VendorMember/VendorMemberProfileView';
+import VendorMemberSiderMenu from '../../components/Screen/VendorMember/VendorMemberSiderMenu'
 import { NavigationContainer } from '@react-navigation/native';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -45,13 +45,14 @@ export default function HomeScreen() {
   return (
 
     <>
+        <View style={styles.Container}>
 
       <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        {/* <Stack.Screen name="SplashScreen" component={SplashScreen} />
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="ConfirmationCode" component={ConfirmationCode} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} /> */}
 
         {/* User Screens */}
         {/* <Stack.Screen name="UserHome" component={UserHome} />
@@ -72,7 +73,7 @@ export default function HomeScreen() {
 
         {/* Admin Screens */}
 
-        {/* <Stack.Screen name="AdminHome" component={AdminHome} /> 
+         {/* <Stack.Screen name="AdminHome" component={AdminHome} /> 
         <Stack.Screen name="EditAdminProfile" component={EditAdminProfile} />
         <Stack.Screen name="AdminProfileView" component={AdminProfileView} />
         <Stack.Screen name="ViewFeedback" component={ViewFeedback} />
@@ -81,41 +82,34 @@ export default function HomeScreen() {
         <Stack.Screen name="AdminSiderMenu" component={AdminSiderMenu} />
         <Stack.Screen name="VendorRegistration" component={VendorRegistration} />
         <Stack.Screen name="ViewVendors" component={ViewVendors} />
-        <Stack.Screen name="ViewUsers" component={ViewUsers} />        */}
+        <Stack.Screen name="ViewUsers" component={ViewUsers} />         */}
 
 
-        {/* Vendor Screens */}
+        {/* VendorMember Screens */}
 
-        {/* <Stack.Screen name="VendorMemberSiderMenu" component={VendorMemberSiderMenu} />
+        <Stack.Screen name="VendorMemberSiderMenu" component={VendorMemberSiderMenu} />
         <Stack.Screen name="AddFoodItems" component={AddFoodItems} />
         <Stack.Screen name="ViewFoodItems" component={ViewFoodItems} />
         <Stack.Screen name="EditVendorMemberProfile" component={EditVendorMemberProfile} />
-        <Stack.Screen name="VendorMemberProfileView" component={VendorMemberProfileView} /> */}
+        <Stack.Screen name="VendorMemberProfileView" component={VendorMemberProfileView} />
 
 
 
       </Stack.Navigator>
 
-
+      </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+
+  Container: {
+    padding: 0,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+    paddingTop: 35,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+
 });
