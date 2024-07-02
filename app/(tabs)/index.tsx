22d7200fe1923d7a44eb3fb6b902c8aec881160a
Darshan magdum <Darshanmagdum79@gmail.com>
2024-07-02 13:24:11 +0530

    cahnhes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -62,14 +62,14 @@ export default function HomeScreen() {
         <Stack.Screen name="UserProfileView" component={UserProfileView} />
         <Stack.Screen name="UserFeedback" component={UserFeedback} /> */}
         {/* Vendor Screens */}
-        {/* <Stack.Screen name="VendorHome" component={VendorHome} />
+        <Stack.Screen name="VendorHome" component={VendorHome} />
         <Stack.Screen name="EditVendorProfile" component={EditVendorProfile} />
         <Stack.Screen name="VendorProfileView" component={VendorProfileView} />
         <Stack.Screen name="VendorSiderMenu" component={VendorSiderMenu} />
         <Stack.Screen name="ManageFoodCollection" component={ManageFoodCollection} />
         <Stack.Screen name="ViewFoodCollection" component={ViewFoodCollection} />
         <Stack.Screen name="AddMembers" component={AddMembers} />
-        <Stack.Screen name="ViewMembers" component={ViewMembers} /> */}
+        <Stack.Screen name="ViewMembers" component={ViewMembers} />
 
         {/* Admin Screens */}
 
@@ -87,11 +87,11 @@ export default function HomeScreen() {
 
         {/* VendorMember Screens */}
 
-        <Stack.Screen name="VendorMemberSiderMenu" component={VendorMemberSiderMenu} />
+        {/* <Stack.Screen name="VendorMemberSiderMenu" component={VendorMemberSiderMenu} />
         <Stack.Screen name="AddFoodItems" component={AddFoodItems} />
         <Stack.Screen name="ViewFoodItems" component={ViewFoodItems} />
         <Stack.Screen name="EditVendorMemberProfile" component={EditVendorMemberProfile} />
-        <Stack.Screen name="VendorMemberProfileView" component={VendorMemberProfileView} />
+        <Stack.Screen name="VendorMemberProfileView" component={VendorMemberProfileView} /> */}
 
 
 
